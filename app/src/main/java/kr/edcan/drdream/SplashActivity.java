798684a6c1529f5f881c662288941fe4e282dd8b
songjun51 <song@wonjun.kr>
2016-11-29 22:49:34 +0200

    Let Splash Skip after 1sec

--- app/src/main/java/kr/edcan/drdream/SplashActivity.java
+++ app/src/main/java/kr/edcan/drdream/SplashActivity.java
@@ -1,5 +1,7 @@
 package kr.edcan.drdream;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,16 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        setInterval();
+    }
+
+    private void setInterval() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 1000);
     }
 }
